5239b04bc69cc135f06f525aee579f170738644a
Hobim <hobimfx@outlook.com>
2025-12-30 00:51:16 +0100

    fix: correct import statement and enhance LoanRecord model with additional fields

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForiegnKey, Float, Datetime
+from sqlalchemy import Column, Integer, String, ForiegnKey, Float, Datetime, Boolean 
 from sqlalchemy.orm  import relationship  
 from datetime import datetime
 from app.db.session import Base  
@@ -16,3 +16,26 @@ class User(Base):
 
 class LoanRecord(Base):
     __tablename__ = 'LoanRecords'
+
+    Age = Column (Integer, index=True, nullable=False)
+    Annual_Income = Column (String, index=True, nullable=False)
+    Loan_Amount = Column(Integer, index=True, nullable=False)
+    Debt_to_income_ratio = Column (Integer, index=True, nullable=False)
+    Education_Level = Column (String, index=True, nullable=False)
+    Type_of_Employment = Column (String, index=True, nullable=False)
+    Marital_Status = Column (String, Index=True, nullable=False)
+    Has_Dependents = Column (Boolean, Index=True, nullable=False)
+    Purpose_of_Loan = Column (String, Index=True, nullable=False)
+    Has_Guarantor = Column(Boolean, Index=True, nullable=False)
+
+
+
+
+
+
+
+
+
+
+
+    
