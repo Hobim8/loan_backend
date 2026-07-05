038cd171c5dfe26c271e58b33490c723f92b7205
Hobim <hobimfx@outlook.com>
2026-07-05 01:48:37 +0100

    fix: correct router inclusion in main application; replace incorrect auth router reference

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 from fastapi import APIRouter
+from core.auth import auth_router
 
-app = FastAPI()  
-auth = APIRouter() 
+app = FastAPI()
 
-app.include_router(auth)  
 
+app.include_router(auth_router)
