06955eece5f596a4586d745d546d9127746c00f0
Hobim <hobimfx@outlook.com>
2026-07-05 01:59:18 +0100

    refactor: streamline password hashing and verification functions; improve formatting for consistency feat: add email-validator dependency for enhanced email validation

--- core/security.py
+++ core/security.py
@@ -1,18 +1,12 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(
-    schemes = ['bcrypt'],
-    deprecation = 'auto'
-)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def hash_password(password : str) -> str:
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def verify_password(password : str,
-                    hashed_password : str) -> bool:
+def verify_password(password: str, hashed_password: str) -> bool:
     return pwd_context.verify(password, hashed_password)
-
-
 
